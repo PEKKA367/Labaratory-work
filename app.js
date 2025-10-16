604454f3dff1c5b8270bc0c52561fee424e1b694
PEKKA367 <ilya.boogai@gmail.com>
2025-10-16 11:45:17 +0300

    fourth commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let pencil_price = 30;
 let pen_price = 30;
 let car_price = 3000000;
+let tomato_price = 4;
+let carrot_price = 5;
 let list = [carrot, potato, tomato];
 
 console.log(pencil_price);
